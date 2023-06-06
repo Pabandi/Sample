a4c40d1859fa614d9439485a300fa608f5773427
Mohammad Akbari <mohammadakbari313@gmail.com>
2023-06-06 10:10:38 +0330

    List of student

--- Sample/Controllers/StudentController.cs
+++ Sample/Controllers/StudentController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Sample.Models;
+
+
 
 namespace Sample.Controllers
 {
     public class StudentController : Controller
     {
+        StudentEntities db = new StudentEntities();
+
+
         // GET: Student
         public ActionResult Index()
         {
-            return View();
+            var students = db.T_Student;
+
+            return View(students);
         }
     }
 }
